5511a464cd806210d3b5f79951c28783e438fa61
Raahul Kumar Prem <raahulprem61@gmail.com>
2021-04-26 11:36:46 +0530

    updated server files

--- app.js
+++ app.js
@@ -21,17 +21,56 @@
 
 // console.log('listening on portport')
 
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+// //   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.pos
+
+// server.get((req, res) => {
+//     if(req.url == '/dataReceive'){
+
+//     }
+// })
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express');
+const server = express();
+
+const sensorData = [
+    {gas: 10, smell: 2, RGB: 15, IR: 10},
+    {gas: 15, smell: 5, RGB: 5, IR: 1},
+];
+
+server.use(express.json());
+
+server.get('/' , (req, res) => {
+    res.send('Hello World')
+})
+
+server.post('/dataViewer', (req, res) => {
+    const sensorData1 = {
+        gas: req.body.gas,
+        smell: req.body.smell, 
+        RGB: req.body.RGB, 
+        IR: req.body.IR
+    }   
+
+    sensorData.push(sensorData1);
+    res.send(sensorData1);
+})
+
+server.get('/dataViewer', (req, res) => {
+    res.send(sensorData)
+})
+server.listen(3000, () => console.log('Listening on port 3000....'))
