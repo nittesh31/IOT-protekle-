37e88101f5e0db152c339025c1770b4005ab8839
SabariDhanush26 <110118071@nitt.edu>
2021-05-21 08:15:41 +0530

    Final Arduino Code

--- arduino/Proteus files/Gas/sketch_apr25f/sketch_apr25f.ino
+++ arduino/Proteus files/Gas/sketch_apr25f/sketch_apr25f.ino
@@ -1,4 +1,4 @@
-int IRSensor = 5; // connect ir sensor to arduino pin 2
+int GasSensor = 5; // connect ir sensor to arduino pin 2
 int LED = 12;
 int LED2 = 11;// conect Led to arduino pin 13
 
@@ -7,25 +7,25 @@ int LED2 = 11;// conect Led to arduino pin 13
 void setup() 
 {
   Serial.begin(9600);
-  pinMode (IRSensor, INPUT); // sensor pin INPUT
+  pinMode (GasSensor, INPUT); // sensor pin INPUT
   pinMode (LED, OUTPUT);
     pinMode (LED2, OUTPUT);// Led pin OUTPUT
 }
 
 void loop()
 {
-  int data = digitalRead (IRSensor);
+  int data = digitalRead (GasSensor);
   if (data==HIGH){
     digitalWrite(LED, HIGH);
     digitalWrite(LED2, LOW); 
-    Serial.println("Gas detected");
+    Serial.print("Gas detected");
   }
   
   if(data==LOW)
   {
     digitalWrite(LED2, HIGH); 
     digitalWrite(LED, LOW); 
-    Serial.println("No Gas detected");
+    Serial.print("No Gas detected");
   }
   delay(100);
 }
